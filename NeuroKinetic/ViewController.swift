fb455646c9c1a5d8efac68f82b6ab7246bcea5de
nmaeda <nmaeda@asb9700-e06.csil.sfu.ca>
2019-10-23 12:50:56 -0700

    Menu implemented

---
+++ NeuroKinetic/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NeuroKinetic
+//
+//  Created by nmaeda on 10/19/19.
+//  Copyright © 2019 teamRANDY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+   
+        
+    
+
+}
+
