ceefc0daeb60244ee26be26ad6b946910984dab7
nmaeda <nmaeda@asb9816-01.csil.sfu.ca>
2019-10-30 09:11:04 -0700

    modification on metronome game

--- NeuroKinetic/ViewController/MetronomeGame/MetronomeGameVC.swift
+++ NeuroKinetic/ViewController/MetronomeGame/MetronomeGameVC.swift
@@ -7,22 +7,23 @@
 //
 
 import UIKit
-
+import AVFoundation
 
 /*
 let timer = Timer.scheduledTimer(TimeInterval: 1, target: self, Selector: #selector(update), userInfo: nil, repeats: true)
 */
 
+/*
 let circleCenter = CGPoint(x: 600,y:600)
 let circleWidth = CGFloat(100)
 let circleHeight = circleWidth
 let circleView = CircleView(frame: CGRect(x: circleCenter.x, y: circleCenter.y, width: circleWidth, height: circleHeight))
 
 var frameView = CircleView(frame: CGRect(x: circleCenter.x, y: circleCenter.y, width: circleWidth, height: circleHeight))
+*/
 
 
-
-
+/*
 func pulse() {
     UIView.animate(withDuration: 0.5, animations:{
         circleView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
@@ -32,50 +33,73 @@ func pulse() {
         })
     })
 }
+*/
 
+func setupButtonStyle(button : UIButton, color: UIColor){
+    // Customizing Menu Button Style
+    button.layer.cornerRadius = 0.5 * button.bounds.size.width
+    button.clipsToBounds = true
+    button.backgroundColor = color
+}
 
 
 class MetronomeGame: UIViewController {
     
-   
-
-    
-    
+    var audioPlayer = AVAudioPlayer()
     var tapCount:Int = 0;
     var str = "0"
     var isWait:Bool = false
+    
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var tapAndwait: UILabel!
     @IBOutlet weak var tap: UIButton!
+    @IBOutlet weak var left: UIButton!
+    @IBOutlet weak var right: UIButton!
+    
+    
     //MARK: Properties
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let sound = Bundle.main.path(forResource:"metronomeSound", ofType: "mp3")
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        }
+        catch{
+            print(error)
+        }
+        left.layer.cornerRadius = 0.5 * left.bounds.size.width
+        left.clipsToBounds = true
+        left.backgroundColor = UIColor.red
+        right.layer.cornerRadius = 0.5 * left.bounds.size.width
+        right.clipsToBounds = true
+        right.backgroundColor = UIColor.red
+        /*
         view.addSubview(circleView)
         frameView.tintColor = UIColor.black
         view.addSubview(frameView)
+        */
     }
     
     @IBAction func TAP(_ sender: UIButton) {
+        //start
         if(tapCount == 0)
         {
-            //label.text = "tapCount == 0"
             tap.setTitle("TAP!", for: .normal)
+            audioPlayer.play()
             //isWait = false
         }
+        //while playing
         else if(!isWait)
         {
-            //TapCount =
             label.text = String(tapCount)
-            pulse()
+            //pulse()
             tap.setTitle("WAIT", for: .normal)
-            //isWait = true
             let seconds = 0.5
             DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
                 self.tap.setTitle("TAP!", for: .normal)
             }
-            //tap.setTitle("TAP!", for: .normal)
-            //isWait = false
-            //*/
         }
         tapCount = tapCount + 1
     }
